6c16da66e8d62983a7b723ecad8c03a6c98e0456
Michael J Rust <ppywmjr@gmail.com>
2017-05-20 16:03:19 +0100

    Fixed downloading and CSS

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-var switcher = "turn on spreadsheet downloader";
+var switcher = "set up spreadsheet downloader";
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: switcher}, function(response) {
+      console.log(switcher);
       console.log(response.farewell);
-      if (switcher == "turn on spreadsheet downloader"){
-        switcher = "turn off" ;
+      if (switcher == "turn off spreadsheet downloader") {
+        switcher =  "turn on spreadsheet downloader";
       }
       else
       {
-      switcher = "turn on spreadsheet downloader";
+      switcher = "turn off spreadsheet downloader";
       }
-
       console.log(switcher);
     });
   });
--- content.js
+++ content.js
@@ -1,15 +1,72 @@
-/*var originalTableColour;
-var orinigalTableHeaderColour;
-var highlighedColour = "#9cbd9a";
-var darkHighlightColour = "#79b076";
+var tablesArray = document.getElementsByTagName("table");
+var containersArray = new Array();
+var tablesHTMLArray = new Array();
 
-function storeOringinalColours(){
-var table0 = nodeListOfTables[0];
-originalTableColour = table0.style.backgroundColor;
-orinigalTableHeaderColour = table0.getElementsByTagName("th")[0].style.backgroundColor;
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if  (request.greeting == "set up spreadsheet downloader"){
+      console.log("runtime.onMessage.setup");
+      addContainersToTables();
+      setUpTablesForDownload();
+      sendResponse({farewell: "byebye"});
+      return true;
+    } else {}
+    if (request.greeting == "turn on spreadsheet downloader"){
+      console.log("runtime.onMessage.turnon");
+      setUpTablesForDownload();
+      sendResponse({farewell: "byebye"});
+      return true;
+    } else {}
+    if (request.greeting == "turn off spreadsheet downloader"){
+      setUpContainersForNoDownload();
+      console.log("else");
+      sendResponse({farewell: "byebye"});
+      return true;
+    } else {}
+});
+
+function setUpTablesForDownload(){
+      console.log("setUpTablesForDownload");
+    addEventListenersToContainers();
 }
-*/
+
+function addContainersToTables(){
+      console.log("addContainersToTables");
+    var numberOfTables = tablesArray.length;
+    for (i = 0; i < numberOfTables; i++) {
+      var thisTable = tablesArray[i];
+      tablesHTMLArray[i] = thisTable.outerHTML;
+      console.log(tablesHTMLArray[i]);
+      addContainerToOneTable(thisTable);
+    }
+}
+
+function addContainerToOneTable(thisTable){
+      console.log("addContainerToOneTable");
+    var thisTableOuterHTML = thisTable.outerHTML;
+    console.log(thisTableOuterHTML);
+    thisTable.outerHTML = '<div class="myContainer">' + thisTableOuterHTML + '<div class="overlay"><img src="chrome-extension://fdocjokhcfnllccciekghidljpjgnjog/icon128.png" class="tableDownloaderIcon"></img></div></div>';
+}
+
+function addEventListenersToContainers(){
+    console.log("addEventListenersToContainers");
+    containersArray = document.getElementsByClassName("myContainer");
+    var numberOfContainers = containersArray.length;
+    for (i = 0; i < numberOfContainers; i++) {
+      var thisContainer = containersArray[i];
+      var tableHTML = tablesHTMLArray[i];
+      thisContainer.addEventListener("click", function(){downloadTable(tableHTML);}, false);
+    }
+}
+
+function downloadTable(tableHTML){
+        console.log("downloadTable");
+    var filename = "table download.xls";
+    download(filename, tableHTML);
+}
+
 function download(filename, text) {
+    console.log("download");
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + text);
     element.setAttribute('download', filename);
@@ -18,48 +75,8 @@ function download(filename, text) {
     element.click();
     document.body.removeChild(element);
 }
+
 /*
-function darkenbackground(){
-  this.style.backgroundColor = darkHighlightColour;
-}
-function lightenBackground(){
-    this.style.backgroundColor = highlighedColour;
-}
-
-function highlightElement(thisElement){
-  thisElement.style.backgroundColor = highlighedColour;
-  thisElement.addEventListener("mouseover", darkenbackground, false);
-  thisElement.addEventListener("mouseout", lightenBackground, false);
-}
-
-function highlightTable(thisTable){
-  highlightElement(thisTable);
-}
-*/
-function setUpTablesForDownload(myNodeList){
-var numberOfNodes = myNodeList.length;
-for (i = 0; i < numberOfNodes; i++) {
-var myTable = myNodeList[i];
-setUpOneTableForDownload(myTable);
-}
-}
-
-function setUpOneTableForDownload(myTable){
-//highlightElement(myTable);
-//myTable.addEventListener("click", downloadTable, false);
-var oldHTML = myTable.outerHTML;
-myTable.outerHTML = '<div class="container">' + oldHTML + '<div class="overlay"><img src="chrome-extension://gcecjnpjjcknlgikoofhjoejegiffpdf/icon128.png" class="tdicon"></img></div></div>';
-myTable.addEventListener("click", downloadTable, false);
-}
-
-
-
-function downloadTable(){
-      var text = this.outerHTML;
-      var filename = "table download.xls";
-      download(filename, text);
-}
-
 function setUpTablesForNoDownload(myNodeList){
 var numberOfNodes = myNodeList.length;
 for (i = 0; i < numberOfNodes; i++) {
@@ -70,41 +87,5 @@ setUpOneTableForNoDownload(myTable);
 
 function setUpOneTableForNoDownload(myTable){
 myTable.removeEventListener("click", downloadTable, false);
-//myTable.removeEventListener("mouseover", darkenbackground, false);
-//myTable.removeEventListener("mouseout", lightenBackground, false);
-}
-
-/*
-function setBackgroundColourByTag(tag, colour){
- var nodeListOfElements = document.getElementsByTagName(tag);
- var numberOfNodes = nodeListOfElements.length;
- for (i = 0; i < numberOfNodes; i++) {
-    var thisChild = nodeListOfElements[i];
-    thisChild.style.backgroundColor = colour;
-//     highlightElement(thisChild);
-}
 }
 */
-var nodeListOfTables = document.getElementsByTagName("table");
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "turn on spreadsheet downloader"){
-//      setBackgroundColourByTag("th", "inherit");
-//      setBackgroundColourByTag("tr", "inherit");
-//      setBackgroundColourByTag("td", "inherit");
-      setUpTablesForDownload(nodeListOfTables);
-      sendResponse({farewell: "byebye"});
-return true;
-}
-else {
-    setUpTablesForNoDownload(nodeListOfTables);
-//    setBackgroundColourByTag("th", orinigalTableHeaderColour);
-//    setBackgroundColourByTag("table", originalTableColour);
-    sendResponse({farewell: "byebye"});
-    return true;
-}
-});
-
-// http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
-// https://www.w3schools.com/howto/howto_css_image_overlay.asp
